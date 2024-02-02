be2085b3eaa8661dd44cc573e7cf2d65d6e3c520
PranavModi999 <pranavmodi999@gmail.com>
2024-02-02 15:56:55 -0500

    added hero section component

--- swift-rentals-frontend/src/app/page.tsx
+++ swift-rentals-frontend/src/app/page.tsx
@@ -2,8 +2,40 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="h-screen w-full bg-pink-200">
-      <section className="bg-red-300 w-72 h-full"></section>
+    <main className="h-screen w-ful">
+      <section className="flex">
+        <div className="w-1/2 bg-red-200 h-screen"></div>
+        <div className="w-1/2 h-screen relative">
+          <Image
+            className="absolute bottom-32 right-72"
+            src={`/images/payment.png`}
+            width={220}
+            height={220}
+            alt="hero payment image"
+          />
+          <Image
+            className="absolute top-12 left-1/2"
+            src={`/images/payment.png`}
+            width={220}
+            height={220}
+            alt="hero payment image"
+          />
+          <Image
+            className="absolute m-3 top-32 left-1/4"
+            src={`/images/profile.png`}
+            width={140}
+            height={140}
+            alt="hero payment image"
+          />
+          <Image
+            className="absolute m-3 bottom-48 right-20"
+            src={`/images/profile.png`}
+            width={140}
+            height={140}
+            alt="hero payment image"
+          />
+        </div>
+      </section>
     </main>
   );
 }
